6cd82eb8fe46111f5223b8277f096f0d5fdb6419
Ranjan M N <ranjanmn.mn1@gmail.com>
2018-05-03 15:02:16 +0530

    category page created

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+var app = express();
+
+
+app.use(express.static(__dirname + '/playground/photography'));
+
+app.get('/', (req , res , next) => {
+    // res.render('./playground/photography/index.html');
+    res.render('index.html');
+    next();
+});
+
+app.listen(3000, () => {
+    console.log('Example app listening to port');
+});
